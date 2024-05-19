efeda2c78bf66711fb74b1d1a823024c3c65a36f
MihailPashin <michalpoisonish@gmail.com>
2024-05-19 22:54:55 +0300

    Docker test. Seven Thousand

--- Containers/DF_Pandas.py
+++ Containers/DF_Pandas.py
@@ -4,7 +4,7 @@ import os
 
 relative_path = '/app/Packages/Loading_DataSet/data/New_coordinates_titles.csv'
 absolute_path = os.path.abspath(os.path.join(os.getcwd(), relative_path))
-
+directory_path = '/app/Packages/Loading_DataSet/data/'
 class Pandas_Container(containers.DeclarativeContainer):
     config = providers.Configuration()
     print(os.getcwd())
