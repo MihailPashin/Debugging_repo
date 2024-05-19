c2f8ed6eb32a2c725817293e8f12ad43cc7da76b
MihailPashin <michalpoisonish@gmail.com>
2024-05-19 22:10:27 +0300

    Absolute Path Pandas, I don't know

--- Containers/DF_Pandas.py
+++ Containers/DF_Pandas.py
@@ -1,11 +1,15 @@
 from dependency_injector import containers, providers
 from Packages.Loading_DataSet.Pandas_ECB_df import DataFrameEntity,DataFrameBoundary,DataFrameControl
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'Packages','Loading_DataSet')))
+
+file_relative_path = os.path.join(os.path.dirname(__file__), '..', 'Packages', 'Loading_DataSet', 'data', 'New_coordinates_titles.csv')
+absolute_file_path = os.path.abspath(file_relative_path)
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 class Pandas_Container(containers.DeclarativeContainer):
     config = providers.Configuration()
-    config.filepath.from_value('data/New_coordinates_titles.csv')
+    config.filepath.from_value(absolute_file_path)
     
     entity = providers.Factory(DataFrameEntity, filepath=config.filepath)
     control = providers.Factory(DataFrameControl, dataframe_entity=entity)
