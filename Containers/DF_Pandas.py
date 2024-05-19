c9934e6796face5997e1db4c3f5d29b1815e6407
MihailPashin <michalpoisonish@gmail.com>
2024-05-19 22:27:38 +0300

    Attempt One-Thousand.Pandas in Docker

--- Containers/DF_Pandas.py
+++ Containers/DF_Pandas.py
@@ -2,14 +2,11 @@ from dependency_injector import containers, providers
 from Packages.Loading_DataSet.Pandas_ECB_df import DataFrameEntity,DataFrameBoundary,DataFrameControl
 import os
 
-file_relative_path = os.path.join(os.path.dirname(__file__), '..', 'Packages', 'Loading_DataSet', 'data', 'New_coordinates_titles.csv')
-absolute_file_path = os.path.abspath(file_relative_path)
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 class Pandas_Container(containers.DeclarativeContainer):
     config = providers.Configuration()
-    config.filepath.from_value(absolute_file_path)
+    config.filepath.from_value('/app/Packages/Loading_DataSet/data/New_coordinates_titles.csv')
     
     entity = providers.Factory(DataFrameEntity, filepath=config.filepath)
     control = providers.Factory(DataFrameControl, dataframe_entity=entity)
