7c3e7da0743bc1f3a65c376acb26d61f66933515
MihailPashin <michalpoisonish@gmail.com>
2024-05-19 22:06:11 +0300

    new link to df

--- Containers/DF_Pandas.py
+++ Containers/DF_Pandas.py
@@ -1,11 +1,11 @@
 from dependency_injector import containers, providers
 from Packages.Loading_DataSet.Pandas_ECB_df import DataFrameEntity,DataFrameBoundary,DataFrameControl
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'Packages','Loading_DataSet')))
 
 class Pandas_Container(containers.DeclarativeContainer):
     config = providers.Configuration()
-    config.filepath.from_value('Packages/Loading_DataSet/data/New_coordinates_titles.csv')
+    config.filepath.from_value('data/New_coordinates_titles.csv')
     
     entity = providers.Factory(DataFrameEntity, filepath=config.filepath)
     control = providers.Factory(DataFrameControl, dataframe_entity=entity)
